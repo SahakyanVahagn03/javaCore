1ca0ace0e1c1e175c1e852bb2312c60e596583e4
Vahag <vahagnsahakyan53@gmail.com>
2023-08-27 11:50:10 -0700

    homework1

---
+++ src/homework/homework1/FigurePainter.java
@@ -0,0 +1,46 @@
+package homework.homework1;
+
+public class FigurePainter {
+
+    public static void main(String[] args) {
+        int length = 7;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < length; i++) {
+            for (int j = length; j > i; j--) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < length; i++) {
+            for (int j = length; j > i + 1; j--) {
+                System.out.print("   ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < length; i++) {
+            for (int j = length; j > i; j--) {
+                System.out.print("  *");
+            }
+            System.out.println();
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("   ");
+            }
+
+        }
+    }
+}
