e3313fcb2ca520b8cb203420b69d0e61a20a5557
Vahag <vahagnsahakyan53@gmail.com>
2023-08-27 13:31:28 -0700

    homework1

--- src/homework/homework1/FigurePainter.java
+++ src/homework/homework1/FigurePainter.java
@@ -42,5 +42,30 @@ public class FigurePainter {
             }
 
         }
+
+        System.out.println();
+
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = length; j > i + 1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < length; i++) {
+            for (int j = length; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" ");
+            }
+
+        }
+
+
+
     }
 }
