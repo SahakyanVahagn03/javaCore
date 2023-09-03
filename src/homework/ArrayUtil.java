27ba541c004dd2b7fdeca524cbabe1e406326740
Vahag <vahagnsahakyan53@gmail.com>
2023-09-03 11:51:03 -0700

    homework3

--- src/homework/ArrayUtil.java
+++ src/homework/ArrayUtil.java
@@ -2,6 +2,7 @@ package homework;
 
 public class ArrayUtil {
     public static void main(String[] args) {
+        //
         int[] numbers = {1, 6, 3, 9, 15, 52, -3, 5, 8};
 
         for (int i = 0; i < numbers.length; i++) {
