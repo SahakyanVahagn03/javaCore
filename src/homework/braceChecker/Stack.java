967988917a5499dd91bab9ea9e8fab6971583d2e
Vahag <vahagnsahakyan53@gmail.com>
2023-09-29 07:54:17 -0700

    I change the stack and pushing again

--- src/homework/braceChecker/Stack.java
+++ src/homework/braceChecker/Stack.java
@@ -1,14 +1,8 @@
 package homework.braceChecker;
 
 public class Stack {
-    //
     private int[] stack = new int[10];
     private int size;
-    private void checkIfNeedToExtend() {
-        if (size == stack.length) {
-            extend();
-        }
-    }
     private void extend() {
         int[] tamp = new int[stack.length + 10];
         if (size >= 0) System.arraycopy(stack, 0, tamp, 0, size);
@@ -18,7 +12,9 @@ public class Stack {
         size = -1;
     }
     public void push(int value) {
-        checkIfNeedToExtend();
+        if (size == stack.length) {
+            extend();
+        }
         stack[++size] = value;
     }
     public int pop() {
@@ -26,14 +22,6 @@ public class Stack {
             return -1;
         }
         return stack[size--];
-    }
-    public boolean exists(int value) {
-        for (int valueOfTheArray : stack) {
-            if (valueOfTheArray == value) {
-                return true;
-            }
-        }
-        return false;
     }
     public int getSize(){
         return size;
