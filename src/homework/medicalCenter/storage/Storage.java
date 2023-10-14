6975656d4ed719ca7691b51be60047c76dc38ac0
Vahag <vahagnsahakyan53@gmail.com>
2023-10-13 19:12:11 -0700

    medicalCenter

--- src/homework/medicalCenter/storage/Storage.java
+++ src/homework/medicalCenter/storage/Storage.java
@@ -1,6 +1,6 @@
 package homework.medicalCenter.storage;
 
-import homework.employee.Employee;
+
 import homework.medicalCenter.model.Doctor;
 import homework.medicalCenter.model.Patient;
 import homework.medicalCenter.model.Person;
