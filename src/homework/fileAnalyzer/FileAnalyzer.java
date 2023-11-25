0c3d2c265e70487e7c19eb53a1be9b2ea27bfe19
Vahag <vahagnsahakyan53@gmail.com>
2023-11-25 05:53:47 -0800

    homework FileAnalyzer

---
+++ src/homework/fileAnalyzer/FileAnalyzer.java
@@ -0,0 +1,100 @@
+package homework.fileAnalyzer;
+
+import com.sun.source.tree.Tree;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+
+public class FileAnalyzer {
+
+    public Map<String, Integer> wordMap(String path) {
+        // Читаем файл, составляем мапу слово-количество и возвращаем ее
+        Map<String, Integer> map = new HashMap<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                if (!map.containsKey(line)) {
+                    map.put(line, 1);
+                } else {
+                    Integer i = map.get(line);
+                    map.replace(line, ++i);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return map;
+    }
+
+    public int totalWordCount(String path) {
+        // Читаем файл, подсчитываем общее количество слов
+        int wordCount = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                ++wordCount;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return wordCount;
+    }
+
+    public int uniqueWordCount(String path) {
+        // Читаем файл, подсчитываем количество уникальных слов
+        Map<String, Integer> words = wordMap(path);
+        int count = 0;
+        for (Integer value : words.values()) {
+            if (value <= 1) {
+                ++count;
+            }
+        }
+
+        return count;
+    }
+
+
+    public Map<String, Integer> topFrequentWords(String path, int n) {
+        // Читаем файл, находим топ-N часто встречающихся слов
+        return sortedList(wordMap(path), n);
+    }
+
+    private Map<String, Integer> sortedList(Map<String, Integer> words, int n) {
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(words.entrySet());
+        list.sort(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        if (list.size() < n) {
+            hashMap.put("txt file is empty", 0);
+        } else {
+            for (int i = 0; i < n; i++) {
+                hashMap.put(list.get(i).getKey(), list.get(i).getValue());
+            }
+        }
+        return hashMap;
+    }
+
+    public int countWordOccurrences(String path, String word) {
+        // Читаем файл, находим количество вхождений слова и возвращаем это число
+        int wordCount = 0;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.equals(word)) {
+                    ++wordCount;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return wordCount;
+    }
+
+}
---
+++ src/homework/fileAnalyzer/FileAnalyzerMain.java
@@ -0,0 +1,77 @@
+package homework.fileAnalyzer;
+
+import java.lang.module.FindException;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Scanner;
+
+public class FileAnalyzerMain {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final FileAnalyzer FILE_ANALYZER = new FileAnalyzer();
+
+    public static void main(String[] args) {
+        boolean run = true;
+        while (run) {
+            System.out.println("select 1 -> which will return map. key(the word in the file) and value(the count of that word)");
+            System.out.println("select 2 - > if you want to Count the total number of words in the file");
+            System.out.println("select 3 - > if you want to count unique Words number ");
+            System.out.println("select 4 - > if you want to find top-n  frequently occurring Words in the file");
+            System.out.println("select 5 - > if you want to search the word and count Word Occurrences");
+            switch (SCANNER.nextLine()) {
+                case "0":
+                    run = false;
+                    break;
+                case "1":
+                    wordMap();
+                    break;
+                case "2":
+                    System.out.println("please write path of the file");
+                    System.out.println("total count of the words in the file is ->  "+ FILE_ANALYZER.totalWordCount(SCANNER.nextLine()));
+                    break;
+                case "3":
+                    System.out.println("please write path of the file");
+                    System.out.println("count of the unique words in the file is -> " + FILE_ANALYZER.uniqueWordCount(SCANNER.nextLine()));
+                    break;
+                case "4":
+                    topFrequentWords();
+                    break;
+                case "5":
+                    countWordOccurrences();
+                    break;
+
+            }
+        }
+    }
+
+    private static void wordMap() {
+        System.out.println("please input path of the file");
+        Map<String, Integer> map = FILE_ANALYZER.wordMap(SCANNER.nextLine());
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    private static void topFrequentWords() {
+        System.out.println("please write path of the file");
+        String path = SCANNER.nextLine();
+        System.out.println("write   top-N, which top-N  frequently occurring words you want to see");
+        String topN = SCANNER.nextLine();
+        if (!topN.chars().allMatch(Character::isDigit)){
+            System.err.println("in the top-N you must write only number ");
+            return;
+        }
+        Map<String, Integer> map = FILE_ANALYZER.topFrequentWords(path, Integer.parseInt(topN));
+        System.out.println("top "+ topN + "  frequently occurring words in the file");
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+
+    private static void countWordOccurrences() {
+        System.out.println("please write path of the file");
+        String path = SCANNER.nextLine();
+        System.out.println("please write the word");
+        String word = SCANNER.nextLine();
+        System.out.println("this word Occurrences "+ FILE_ANALYZER.countWordOccurrences(path, word) + " times");
+    }
+}
